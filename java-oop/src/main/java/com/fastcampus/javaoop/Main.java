4760f5453a1a102cf997a5acc712a2237b6f0f2c
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-01-24 22:25:32 +0900

    Config #4 - spring configuration, bean 등록
    루트 패키지 이하에 bean 을 등록, 있는지 스캔한다.

--- java-oop/src/main/java/com/fastcampus/javaoop/Main.java
+++ java-oop/src/main/java/com/fastcampus/javaoop/Main.java
@@ -1,17 +1,24 @@
 package com.fastcampus.javaoop;
 
 
+import com.fastcampus.javaoop.config.Config;
 import com.fastcampus.javaoop.logic.BubbleSort;
 import com.fastcampus.javaoop.logic.JavaSort;
 import com.fastcampus.javaoop.logic.Sort;
+import com.fastcampus.javaoop.service.SortService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args){
-        Sort<String> sort = new JavaSort<>();
-
-        System.out.println("[result]" + sort.sort(Arrays.asList(args)));
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(Config.class);
+        SortService sortService = context.getBean(SortService.class);
+        System.out.println(
+                "[result]"
+                        + sortService.doSort(Arrays.asList(args)));
     }
 }
--- java-oop/src/main/java/com/fastcampus/javaoop/Service/SortService.java
+++ java-oop/src/main/java/com/fastcampus/javaoop/Service/SortService.java
@@ -1,14 +1,18 @@
-package com.fastcampus.javaoop.Service;
+package com.fastcampus.javaoop.service;
 import com.fastcampus.javaoop.logic.JavaSort;
 import com.fastcampus.javaoop.logic.Sort;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class SortService {
 
     private final Sort<String> sort;
 
-    public SortService(Sort<String> sort){
+    public SortService(@Qualifier("bubbleSort") Sort<String> sort){
+        // bean 이 두개라고 뜨면 @Qualifier
         this.sort = sort;
         System.out.println("구현체: " + sort.getClass().getName());
     }
