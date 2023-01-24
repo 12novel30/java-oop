cf159c923ecc5b1b454d8ec3a165832983c3d06a
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-01-24 14:37:57 +0900

    Implement javaSort
    자바 정렬 구현체를 단순히 이용한 정렬 클래스 구현

---
+++ java-oop/src/test/java/com/fastcampus/javaoop/logic/JavaSortTest.java
@@ -0,0 +1,23 @@
+package com.fastcampus.javaoop.logic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaSortTest {
+    @Test
+    @DisplayName("자바 정렬: 리스트를 넣으면 정렬된 결과를 리턴")
+    void given_List_WhenExecuting_ThenReturnSortedList(){
+        // given
+        JavaSort<Integer> javaSort = new JavaSort<>();
+
+        // when
+        List<Integer> actual = javaSort.sort(List.of(3,2,4,5,1));
+
+        // then
+        assertEquals(List.of(1,2,3,4,5), actual);
+    }
+}
