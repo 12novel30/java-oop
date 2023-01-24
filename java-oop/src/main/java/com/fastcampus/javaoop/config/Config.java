5df9fcdfb4385bda30bfd9be6f471d63d2330dae
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-01-24 22:24:45 +0900

    Config #4 - spring configuration
    기초 설정

---
+++ java-oop/src/main/java/com/fastcampus/javaoop/config/Config.java
@@ -0,0 +1,10 @@
+package com.fastcampus.javaoop.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.fastcampus.javaoop")
+@Configuration
+public class Config {
+
+}
